db2fb29ed09a7b6eda88cab77f889e1609cf8200
Pulin <pulin4122001@gmail.com>
2023-03-31 23:09:35 +0200

    initial auth added

---
+++ user-service/src/main/java/com/app/curioq/userservice/controller/AuthenticationController.java
@@ -0,0 +1,33 @@
+package com.app.curioq.userservice.controller;
+
+import com.app.curioq.userservice.model.AuthenticationRequestDTO;
+import com.app.curioq.userservice.model.AuthenticationResponseDTO;
+import com.app.curioq.userservice.model.RegisterRequestDTO;
+import com.app.curioq.userservice.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponseDTO> register(@RequestBody RegisterRequestDTO request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponseDTO> authenticate(@RequestBody AuthenticationRequestDTO request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+}
