d7431703630ec1aaa8474a12319c778681489486
Pulin <pulin4122001@gmail.com>
2023-04-06 23:22:00 +0200

    user follow implemented

--- user-service/src/main/java/com/app/curioq/userservice/userservice/config/UserCommandLineRunner.java
+++ user-service/src/main/java/com/app/curioq/userservice/userservice/config/UserCommandLineRunner.java
@@ -21,29 +21,18 @@ public class UserCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        userService.register(RegisterRequestDTO.builder()
-                .firstname("admin")
-                .lastname("last1")
-                .email("admin@gmail.com")
-                .password("pass")
-                .role(Role.ADMIN)
-                .build());
+        userService.register(createUser("user1", "last1", "user1@gmail.com", Role.USER.name()));
+        userService.register(createUser("user2", "last2", "user2@gmail.com", Role.USER.name()));
+        userService.register(createUser("user3", "last3", "user3@gmail.com", Role.USER.name()));
+        userService.register(createUser("user4", "last4", "user4@gmail.com", Role.USER.name()));
+        userService.register(createUser("user5", "last5", "user5@gmail.com", Role.USER.name()));
+        userService.register(createUser("user6", "last6", "user6@gmail.com", Role.USER.name()));
+        userService.register(createUser("user7", "last7", "user7@gmail.com", Role.USER.name()));
+        userService.register(createUser("user8", "last8", "user8@gmail.com", Role.USER.name()));
+        userService.register(createUser("user9", "last9", "user9@gmail.com", Role.USER.name()));
+        userService.register(createUser("user10", "last10", "user10@gmail.com", Role.USER.name()));
 
-        userService.register(RegisterRequestDTO.builder()
-                .firstname("user1")
-                .lastname("last2")
-                .email("user1@gmail.com")
-                .password("pass")
-                .role(Role.USER)
-                .build());
-
-        userService.register(RegisterRequestDTO.builder()
-                .firstname("user2")
-                .lastname("last3")
-                .email("user2@gmail.com")
-                .password("pass")
-                .role(Role.USER)
-                .build());
+        userService.register(createUser("admin", "last1", "admin@gmail.com", Role.ADMIN.name()));
 
         AuthenticationResponseDTO responseDTO = userService.login(
                 AuthenticationRequestDTO.builder()
@@ -53,5 +42,15 @@ public class UserCommandLineRunner implements CommandLineRunner {
 
         log.info("USER COMMAND LINE RUNNER ::: Auto login for ADMIN, token - {}", responseDTO.getToken());
         log.info("USER COMMAND LINE RUNNER ::: Initial Data Load Successful");
+    }
+
+    private RegisterRequestDTO createUser(String firstName, String lastName, String email, String role){
+        return RegisterRequestDTO.builder()
+                .firstname(firstName)
+                .lastname(lastName)
+                .email(email)
+                .password("pass")
+                .role(Role.valueOf(role))
+                .build();
     }
 }
